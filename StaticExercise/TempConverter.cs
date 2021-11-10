c859b625bbe6fb8df1957c793874a0c9dac9812f
justin long <justinxlong93@gmail.com>
2021-11-09 18:19:54 -0600

    Initial commit

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        }
+    }
+}
